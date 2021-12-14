b124d87f92517ab1747180087d6a90a01295a0c1
Mark <markcfleming1990@gmail.com>
2021-12-14 16:23:09 -0500

    saved progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,8 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskhandler = function(event) {
 
   event.preventDefault();
-  
+  var taskNameInput = document.querySelector("input[name='task-name']");
+  console.log(taskNameInput);
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
